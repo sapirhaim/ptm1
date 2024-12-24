106b51f34583ae38b01dbd42b09638b1ccef35a1
sapirhaim <sapirhaim11@gmail.com>
2024-12-24 22:25:07 +0200

    initial files

---
+++ Message.java
@@ -0,0 +1,12 @@
+package test;
+
+import java.util.Date;
+
+public class Message {
+    public final byte[] data;
+    public final String asText;
+    public final double asDouble;
+    public final Date date;
+
+
+}
