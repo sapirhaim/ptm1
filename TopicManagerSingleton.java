883d141df553fa580cdb9543af85d3a46a0c980a
sapirhaim <sapirhaim11@gmail.com>
2025-01-05 19:07:24 +0200

    Update TopicManagerSingleton.java

--- TopicManagerSingleton.java
+++ TopicManagerSingleton.java
@@ -1,10 +1,36 @@
 package test;
-
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TopicManagerSingleton {
 
-    public static class TopicManager{
+    public static class TopicManager {
+
+        Map<String, Topic> topics;
+        private static final TopicManager instance = new TopicManager(); // וחייב לאתחל הוא לא חשוף החוצה
+
+        private TopicManager() { // בנאי פרטי
+        topics = new HashMap<>();
+        }
+
+        public synchronized Topic getTopic(String topicName) {
+            if(!topics.containsKey(topicName)) {
+                topics.put(topicName, new Topic(topicName));
+            }
+            return topics.get(topicName);
+        }
+
+        public Collection<Topic> getTopic() {
+            return topics.values();
+        }
+
+        public void clear() {
+            topics.clear();
+        }
     }
 
-    
+    public static TopicManager get() {
+        return TopicManager.instance;
+    }
 }
